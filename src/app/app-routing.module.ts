c21d058f9689e9d47e75c59f7415269c9c9ecb18
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-11-21 20:15:27 -0500

    Se crearon las rutas para heroes y about y se les puso el [routerLink]=['home'] con su respectiva opcion en el navbar a cada ruta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 //Components
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
+
 ];
 
 @NgModule({

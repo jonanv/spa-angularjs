8e945619f1202d08f155172fb24e943ebdd96abd
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-11-21 19:58:45 -0500

    Se crearon las rutas para el home y general que redirecciona al home en app-routing.module.ts y se llamo en el app.component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+//Components
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

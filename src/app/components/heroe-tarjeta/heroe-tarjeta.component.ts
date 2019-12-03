a08b722a02980bd814d2cfa51f7121aedc12b595
Giovanni Vargas <jonan-vargas23@hotmail.com>
2019-12-02 22:08:26 -0500

    Se creo un nuevo componente heroeTarjeta y se reutilizo en heroes con envio de parametros de un componente padre a un componente hijo con el decorador @Input

---
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Heroe } from '../../interfaces/heroe.interface';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html',
+  styleUrls: ['./heroe-tarjeta.component.css']
+})
+export class HeroeTarjetaComponent implements OnInit {
+
+  @Input() heroe: Heroe;
+  @Input() idx: number;
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  verHeroe() {
+    this.router.navigate(['/heroe', this.idx]);
+  }
+}
